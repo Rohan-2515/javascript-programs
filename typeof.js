b8ac652b8956db6f5d85525fad0904135a23875f
Rohan-2515 <roohangaikwad1111@gmail.com>
2024-07-31 19:29:20 +0530

    31-7-24:Assign2-Datatype and it's Typeof
    31-7-24:Assign2-Datatype and it's Typeof

---
+++ typeof.js
@@ -0,0 +1,34 @@
+console.log("----------------------------1--------------------------------------");
+var bankname;
+console.log("Bank-Name Before Intilize:",bankname);
+bankname = "SBI-Bank";
+console.log("Bank-name after Intilization:",bankname);
+console.log("Bank-Name Datatype is:", typeof bankname);
+
+console.log("----------------------------2--------------------------------------");
+
+var marks = "91";
+console.log("My Marks are:", marks);
+console.log("My Marks datatype is:",typeof marks);
+
+console.log("----------------------------3--------------------------------------");
+
+var isworking;
+console.log("My Working Status is", isworking);
+isworking = true;
+console.log("My Working Status is:", isworking);
+console.log("My Working Status Datatype is:", typeof isworking);
+
+console.log("----------------------------4(a)--------------------------------------");
+
+var totalCount = "Hundred and Seven"
+console.log("total count is:",totalCount);
+console.log("totalcount datatype is:", typeof totalCount);
+
+console.log("----------------------------4(b)--------------------------------------");
+
+totalCount = 107;
+console.log("Total-Count after updating is:", totalCount);
+console.log("Total-Count datatype after updating:", typeof totalCount);
+
+
