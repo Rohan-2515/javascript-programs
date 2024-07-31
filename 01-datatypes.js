e2cf27d7c87c53c5541735353658b10059b2d93a
Rohan-2515 <roohangaikwad1111@gmail.com>
2024-07-31 18:25:05 +0530

    Javascript data-types program
    Javascript data-types program

---
+++ 01-datatypes.js
@@ -0,0 +1,30 @@
+//primitive data types
+var age = 22;
+console.log("Age is:",age);
+console.log("type of age is:", typeof age);
+
+console.log("==================string datatypes====================");
+
+var city = "Sangola";
+console.log("city is: ",city);
+console.log("type of city  is:", typeof city);
+
+var marks = "90";
+console.log("marks=:",marks);
+console.log("type of marks is:", typeof marks);
+
+console.log("==========Boolean datatype===========");
+
+var ismarried = true;
+console.log("is married :", ismarried);
+console.log("typeof of ismarried", typeof ismarried);
+
+console.log("============special datatype==============");
+
+var Sangola;
+console.log("SANGOLA VALUE IS:", Sangola);
+console.log("TYPEOF SANGOLA IS:", typeof Sangola);
+
+var kadlas = null;
+console.log("sangola is:", kadlas);
+console.log("type of kadlas  is: ", typeof kadlas);
