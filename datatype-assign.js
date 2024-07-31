4e8cbc4ed67a7ff96661c66e7f82908a3c966fce
Rohan-2515 <roohangaikwad1111@gmail.com>
2024-07-31 18:57:47 +0530

    30-7-24:Assi-Datatype  and varibale program
    30-7-24:Assi-Datatype  and varibale program

---
+++ datatype-assign.js
@@ -0,0 +1,31 @@
+var myfullname;
+console.log("My Name before intialization",myfullname);
+myfullname = "Rohan  Gaikwad";
+console.log("My first and Last Name is after intilaization:",myfullname);
+myfullname = "Rohan Tanaji Gaikwad";
+console.log("My Full Name is after updating:", myfullname);
+
+console.log("==============================================================");
+console.log("I Love only JS");
+console.log("==============================================================");
+var firstname = "Rohan";
+
+console.log("My first Name is:", firstname);
+
+var lastname = "Gaikwad";
+console.log("My Last Name is:", lastname);
+
+var age = 21;
+console.log("My Age is:", age);
+
+var address= "AT/Post, Kadlas, CH. Shivaji nagar jath road";
+console.log("My address is:", address);
+
+var pincode = 413309;
+console.log("Pincode is:", pincode);
+
+var gender = "male";
+console.log("Gender:", gender);
+
+var mar_status = "False";
+console.log("Marriage Status", mar_status);
