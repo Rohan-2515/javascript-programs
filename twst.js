0045c419b23766929ada288dc192818677b447a8
Rohan-2515 <roohangaikwad1111@gmail.com>
2024-07-31 16:59:50 +0530

    javascript-first program
    javascript-first program

---
+++ twst.js
@@ -0,0 +1,5 @@
+console.log("Hello,i am Rohan");
+console.log("my city name is Sangola");
+console.log("My best friend is pratik");
+console.log("My school name is sahyadri Endlish Medium School");
+console.log("My college name is vivekanand College kolhapur");
