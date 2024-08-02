57847e94c0bc0e97a79a1ba1ca9ff673405b9360
Rohan-2515 <roohangaikwad1111@gmail.com>
2024-08-02 15:19:46 +0530

    02-08-2024: Javascript-Lecture Programs of functions
    02-08-2024: Javascript-Lecture Programs of functions

---
+++ 03-funAssign01.js
@@ -0,0 +1,29 @@
+console.log("==============================1===============================");
+function first(){
+    console.log("Rohan");
+}
+first();
+
+function second(){
+    console.log("Gaikwad");
+}
+second();
+
+console.log("==============================2===============================");
+
+function personalDetail(firstName, LastName, CollegeName){
+    console.log("My FirstName is:",firstName);
+    console.log("My lastName is:",LastName);
+    console.log("My collegeName is:",CollegeName);
+}
+personalDetail("Rohan", "Gaikwad", "Vivekanada College Kolhapur");
+
+console.log("==============================3===============================");
+ 
+function addthreevalues(n1, n2, n3){
+    var add = n1 + n2 + n3;
+    console.log("Addition is:",add);
+    console.log("Addition is:","Hello", "Good", "Morning")
+
+}
+addthreevalues(10.23, 600, 40);
