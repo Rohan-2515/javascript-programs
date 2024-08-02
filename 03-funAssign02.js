1883733e4a41396896e1c677c5d9f66633972b56
Rohan-2515 <roohangaikwad1111@gmail.com>
2024-08-02 12:39:17 +0530

    02-08-24: Javascript functions- functions with argument all methonds
    02-08-24: Javascript functions- functions with argument all methonds

---
+++ 03-funAssign02.js
@@ -0,0 +1,8 @@
+console.log("==============================1==============================");
+
+function bankDetails(bankName, accountNum, location, pinCode){
+    console.log("bankName :",bankName, "accountNum:",accountNum, "Location:",location, "pinCode:",pinCode );
+}
+bankDetails("CITI Bank ||", "345678345 ||", "Pune ||", "431202");
+bankDetails("Axis-Bank ||", "7856782345 ||", "Mumbai ||", "441202");
+bankDetails(" HDFC-Bank ||", " 8956782345||", "Pune ||", "631202");
