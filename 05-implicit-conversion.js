8fb16bf048bd7c32e91f6aab22c707ba3722d124
Rohan-2515 <roohangaikwad1111@gmail.com>
2024-08-07 15:18:30 +0530

    05-operatorAssignC-Changes-varibale name
    05-operatorAssignC-Changes-varibale name

---
+++ 05-implicit-conversion.js
@@ -0,0 +1,24 @@
+let result;
+result = '3' + 2;
+console.log("Result:",result);
+
+console.log("==========================================");
+
+
+result = '3' + true;
+console.log("Result: ",result);
+
+console.log("==========================================");
+
+result = '3' + undefined;
+console.log("Result: ",result);
+
+console.log("==========================================");
+
+result = '3' + null;
+console.log("Result:",result);
+
+
+
+
+
