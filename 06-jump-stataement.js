ee1d3eeb5ba66645ed9ad3e9e94156de8e861636
Rohan-2515 <roohangaikwad1111@gmail.com>
2024-08-11 16:07:46 +0530

    06-jump-statement-break and continue
    06-jump-statement-break and continue

---
+++ 06-jump-stataement.js
@@ -0,0 +1,16 @@
+for(let index = 0; index <= 20; index++){
+    console.log(index);
+    if (index == 6) {
+        break;
+    }
+
+
+for(let index = 0; index <= 5; index++){
+    console.log(index);
+    
+    if (index == 2) {
+        continue;
+    }
+    console.log(`=====================`);
+    
+}
